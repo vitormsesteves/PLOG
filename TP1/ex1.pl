e6c819340ea35652938159dea6134bbb0485c5d0
atomicscale <up201303104@fe.up.pt>
2016-10-10 00:05:04 +0100

    added ex1, working on tp1

---
+++ TP1/ex1.pl
@@ -0,0 +1,19 @@
+male('Aldo Burrows').
+male('Lincoln Burrows').
+male('Michael Scofield').
+male('LJ Burrows').
+female('Christina Rose Scofield').
+female('Lisa Rix').
+female('Sara Tancredi').
+female('Ella Scofield').
+
+parent('Lincoln Burrows', 'Aldo Burrows').
+parent('Lincoln Burrows', 'Christina Rose Scofield').
+parent('Michael Scofield', 'Aldo Burrows').
+parent('Michael Scofield', 'Christina Rose Scofield').
+
+parent('LJ Burrows', 'Lisa Rix').
+parent('LJ Burrows', 'Lincoln Burrows').
+
+parent('Ella Scofield', 'Michael Scofield').
+parent('Ella Scofield', 'Sara Tancredi').
