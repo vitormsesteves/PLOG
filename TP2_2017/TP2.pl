c1c81c8a6184391bb0f515a115035216e7dd427c
Vitor Esteves <up201303104@fe.up.pt>
2017-12-25 08:42:52 +0000

    finished

--- TP2_2017/TP2.pl
+++ TP2_2017/TP2.pl
@@ -1,46 +1,45 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
-:- use_module(library(random)).
 
-find_optimal(Group, GroupLength, Initial, ChairsPerRow, Vars, VT):-
+optimal(Group, GroupLength, Initial, RowSize, Vars, Variation):-
 	minimum(MinSeat,Group),
 	maximum(MaxSeat,Group),
 	(MaxSeat - MinSeat) #= (GroupLength - 1), 
 
-	((MaxSeat-1) mod ChairsPerRow) #> ((MinSeat-1) mod ChairsPerRow), %Must be on the same row - This implementation means that a group mustnt be bigger than a row
+	((MaxSeat-1) mod RowSize) #> ((MinSeat-1) mod RowSize),
 
 	variation_list(Group, Initial, Vars),
-	sum(Vars, #= , VT).
+	sum(Vars, #= , Variation).
 	
 variation_list([], [], []).
 variation_list([G|Group], [I|Initial], [V|Vars]):-
-	(abs(G-I) #\= 0 #<=> V), %ou (V #= abs(G-I)), com a primeira, obtem-se o menor numero de mudanças, com a segunda a mudança mas pequena
+	(abs(G-I) #\= 0 #<=> V), %ou (V #= abs(G-I)), com a primeira, obtem-se o menor numero de mudanças, com a segunda a mudança mais pequena
 	variation_list(Group, Initial, Vars).
 
 
-%VT = minimo de variação, Vars, Quais variaram a sua posiçao
-find_optimal_groups(_, [], _ , [], []).
-find_optimal_groups([Initial|IRest], [Group|GRest], ChairsPerRow, [Var|VRest], [VT|VTRest]) :-
+%Variation = minimo de variação, Vars, Quais variaram a sua posiçao
+optimal_groups(_, [], _ , [], []).
+optimal_groups([Initial|IRest], [Group|GRest], RowSize, [Var|VRest], [Variation|VTRest]) :-
 	length(Initial,GroupLength),
-	find_optimal(Group, GroupLength, Initial, ChairsPerRow, Var, VT),
-	find_optimal_groups(IRest, GRest, ChairsPerRow, VRest, VTRest).
+	optimal(Group, GroupLength, Initial, RowSize, Var, Variation),
+	optimal_groups(IRest, GRest, RowSize, VRest, VTRest).
 
 %Se o grupo nao couber na fila, começará a ser ordenado na fila seguinte. 
-%Para grupos > que ChairsPerRow, começaram a ser ordenados a partir do inicio da primeira row nao ocupada. Neste caso nao é tido em conta VT
-manyGroupRedis(Seats, ChairsPerRow, Initials, Groups, Vars, VT) :-
+%Para grupos > que RowSize, começaram a ser ordenados a partir do inicio da primeira row nao ocupada. Neste caso nao é tido em conta VT
+solve_group_destribution(Seats, RowSize, Initials, Groups, Vars, Variation) :-
 	statistics(runtime, [T0| _]),
 	nl,
 	loading,
 	length(Initials, GroupNum),
 	length(Groups, GroupNum),
 	initialize(Groups, Initials, Seats),
-	find_optimal_groups(Initials, Groups, ChairsPerRow, Vars, VTList),
+	optimal_groups(Initials, Groups, RowSize, Vars, VTList),
 	append(Groups,FlattenedGroups),
 	all_distinct(FlattenedGroups),
-	sum(VTList, #= , VT),
-	labeling([minimize(VT),time_out(20000,_)],FlattenedGroups),
+	sum(VTList, #= , Variation),
+	labeling([minimize(Variation),time_out(20000,_)],FlattenedGroups),
 	cls,
-	outputseats(1, ChairsPerRow, Seats, Groups, []),
+	outputseats(1, RowSize, Seats, Groups, []),
     statistics(runtime, [T1|_]),
     T is T1 - T0,
     nl,nl,
@@ -54,25 +53,25 @@ initialize([Group|GRest], [Initial|IRest], Seats) :-
 	initialize(GRest, IRest, Seats).
 
 writeOutput(_, _, []).
-writeOutput(RowCounter, ChairsPerRow, [Seat|List]):-
+writeOutput(RowCounter, RowSize, [Seat|List]):-
 	(
-		RowCounter == ChairsPerRow, nl, write(' '), write(Seat), writeOutput(1, ChairsPerRow, List)
+		RowCounter == RowSize, nl, write(' '), write(Seat), writeOutput(1, RowSize, List)
 	);
-	write(' '), write(Seat), NewRowCounter is RowCounter+1, writeOutput(NewRowCounter, ChairsPerRow, List).
+	write(' '), write(Seat), NewRowCounter is RowCounter+1, writeOutput(NewRowCounter, RowSize, List).
 	
 isFromGroupNumber(_, [], _, _):- fail.
 isFromGroupNumber(Counter, [Group|Groups], N, Number):-
 	member(Counter, Group), Number = N;
 	N1 is N+1, isFromGroupNumber(Counter, Groups, N1, Number).
 	
-outputseats(_, ChairsPerRow, 0, _, OutputList):- nl, writeOutput(0, ChairsPerRow, OutputList).
-outputseats(Counter, ChairsPerRow, Seats, Groups, OutputList):-
+outputseats(_, RowSize, 0, _, OutputList):- nl, writeOutput(0, RowSize, OutputList).
+outputseats(Counter, RowSize, Seats, Groups, OutputList):-
 	(
 		isFromGroupNumber(Counter, Groups, 1, Number), append(OutputList, [Number], NewList);
 		append(OutputList, ['_'], NewList)
 	),	
 	NewCounter is Counter+1, NewSeats is Seats-1,
-	outputseats(NewCounter, ChairsPerRow, NewSeats, Groups, NewList).
+	outputseats(NewCounter, RowSize, NewSeats, Groups, NewList).
 
 cls :- write('\e[2J').
 loading:- write('\t'), write('\t'), write('\t'), write('\t'), write('\t'), write('Loading').
